a26456f3382d7b385b58fba82c1f6846f3646922
andreasnarep <andreasnarep2@gmail.com>
2023-12-10 12:51:47 +0200

    Faililugeja bug parandus

--- Faililugeja.py
+++ Faililugeja.py
@@ -1,37 +1,27 @@
 import pandas as pd
 import json
 
-# Takes a file path and reads the json data into a Pandas DataFrame
-def ReadIntoDataframe(file):
 
+# Takes a file path and reads the json data into a Pandas DataFrame
+def ReadIntoDataframe(file, target_year):
     titles = []
     bodys = []
     dates = []
     columns = ["Title", "Body", "Date"]
 
-    months = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"]
-    days = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"]
-
     with open(file) as f:
         data = json.load(f)
 
-        for v in data.values():
-            for month in months:
-                for day in days:
-                    try:
-                        innder_data = v[month]
-                    except KeyError: # This month does not exist in the data
-                        break
-                    try:
-                        innder_data = innder_data[day]
-                    except KeyError: # This day does not exist in the data
-                        continue
-
-                    for el in innder_data:
-                        titles.append(el["päis"].strip())
-                        bodys.append(el["sisu"].strip())
-                        dates.append("2023-{0}-{1}".format(month, day)) # Dates format - YYYY-MM-DD
-                                                                        # TODO - figure out how the choosing of year is managed
+        for year, months in data.items():
+            if year == target_year:
+                for month, days in months.items():
+                    for day, articles in days.items():
+                        for article in articles:
+                            title = article.get("päis", "")
+                            body = article.get("sisu", "")
+                            titles.append(title)
+                            bodys.append(body)
+                            dates.append("{0}-{1}-{2}".format(year, month, day))
 
     df = pd.DataFrame(list(zip(titles, bodys, dates)), columns=columns)
     return df
--- Terminite_Kasutus.py
+++ Terminite_Kasutus.py
@@ -4,33 +4,92 @@ from estnltk import Text
 from collections import Counter
 import matplotlib.pyplot as plt
 
-# Columns = ["Title", "Body", "Date"]
-faili_nimed = ["ERR_Artiklid.json"]
+faili_nimed = ["ERR_Artiklid_Umbes_Kolm_Aastat/ERR_Artiklid.json"]
 
-huvipakkuvad_sonad = ["chatgpt", "suitsiid", "enesetapp", "tehisintellekt"]
-counts = Counter({'chatgpt': 0, 'suitsiid': 0, 'enesetapp': 0, 'tehisintellekt': 0})
-#failinimi = "ERR_Artiklid.json"
+huvipakkuvad_sonad = [
+    "chatgpt",
+    "suitsiid",
+    "enesetapp",
+    "tehisintellekt",
+    "pandeemia",
+    "valitsus",
+    "5g",
+    "vandenõuteooria",
+    "sõda",
+    "ukraina",
+    "nutitelefon",
+    "telefon",
+    "krüpto",
+    "krüptovaluuta",
+    "annekteerima",
+    "tuumasõda",
+    "kaugtöö",
+    "kodukontor",
+    "distantsõpe",
+    "kaugõpe",
+    "bitcoin",
+    "vr",
+    "virtuaalreaalsus",
+]
+counts = Counter(
+    {
+        "chatgpt": 0,
+        "suitsiid": 0,
+        "enesetapp": 0,
+        "tehisintellekt": 0,
+        "pandeemia": 0,
+        "valitsus": 0,
+        "5g": 0,
+        "vandenõuteooria": 0,
+        "sõda": 0,
+        "ukraina": 0,
+        "nutitelefon": 0,
+        "telefon": 0,
+        "krüpto": 0,
+        "krüptovaluuta": 0,
+        "annekteerima": 0,
+        "tuumasõda": 0,
+        "kaugtöö": 0,
+        "kodukontor": 0,
+        "distantsõpe": 0,
+        "kaugõpe": 0,
+        "bitcoin": 0,
+        "vr": 0,
+        "virtuaalreaalsus": 0,
+    }
+)
+# failinimi = "ERR_Artiklid.json"
+j = 0
 
+output_failid = ["2021_artiklid.csv"]
+aastad = ["2021"]
 
 for failinimi in faili_nimed:
-    df = Faililugeja.ReadIntoDataframe(failinimi)
-    #print(df["Date"].unique())
+    df = Faililugeja.ReadIntoDataframe(failinimi, aastad[j])
+    print(df.head())
+    i = 0
 
     for index, row in df.iterrows():
+        if i % 2000 == 0:
+            print(i)
+
         text = Text(row["Body"])
         text = text.tag_layer()
         text = text.morph_analysis
 
         for el in text.lemma:
             for lemma in el:
-                if (lemma.lower() in huvipakkuvad_sonad):
+                if lemma.lower() in huvipakkuvad_sonad:
                     counts.update([lemma.lower()])
 
-    with open("test.tsv", "w") as f:
+        i += 1
+
+    with open(output_failid[j], "w") as f:
         for k, v in counts.most_common():
-            f.write("{0}\t{1}\n".format(k, v))
+            f.write("{0},{1}\n".format(k, v))
 
-    print(counts)
+    j += 1
 
-plt.bar(*zip(*counts.items()))
-plt.show()
+print(i)
+# plt.bar(*zip(*counts.items()))
+# plt.show()
